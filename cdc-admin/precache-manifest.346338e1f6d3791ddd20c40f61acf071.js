a7ef52835d1671e60fc16680a97c86656a634111
ribeirogab <gblosr@gmail.com>
2020-02-18 16:39:22 -0300

    cdc-admin project added

---
+++ cdc-admin/precache-manifest.346338e1f6d3791ddd20c40f61acf071.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f5aec83f93252c2a3ad884bace2cd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eb214dd0cff1b5b9a27",
+    "url": "/static/css/main.0fc4df82.chunk.css"
+  },
+  {
+    "revision": "6d963a74bf886d09b081",
+    "url": "/static/js/2.3230fce6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.3230fce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb214dd0cff1b5b9a27",
+    "url": "/static/js/main.aa169ad0.chunk.js"
+  },
+  {
+    "revision": "97a449124596d1d47efa",
+    "url": "/static/js/runtime-main.479d106f.js"
+  }
+]);
