b784c3e35d15a19a0cab4b5427ddf4beaca88fc3
ribeirogab <gblosr@gmail.com>
2020-02-18 21:10:40 -0300

    .

---
+++ cdc-admin/precache-manifest.7c7315eb25fa20e356ee2aa1743a446c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4340fb4a9043b95f223558d0a379a1ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4c6aad44c11bf9c1e69",
+    "url": "/static/css/main.0fc4df82.chunk.css"
+  },
+  {
+    "revision": "6d963a74bf886d09b081",
+    "url": "/static/js/2.3230fce6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.3230fce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4c6aad44c11bf9c1e69",
+    "url": "/static/js/main.1cf5a0b8.chunk.js"
+  },
+  {
+    "revision": "97a449124596d1d47efa",
+    "url": "/static/js/runtime-main.479d106f.js"
+  }
+]);
--- cdc-admin/service-worker.js
+++ cdc-admin/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cdc-admin/precache-manifest.396baed6706113f7b4735143544e6962.js"
+  "/precache-manifest.7c7315eb25fa20e356ee2aa1743a446c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cdc-admin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
